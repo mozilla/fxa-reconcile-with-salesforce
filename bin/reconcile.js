0b1ff3810eba65a171799d06e596ea350409fd7a
Shane Tomlinson <stomlinson@mozilla.com>
2018-05-10 17:14:19 +0100

    Only reconcile.js now

---
+++ bin/reconcile.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const program = require('commander');
+
+const CSVReader = require('../lib/readers/csv-reader');
+const DiffReader = require('../lib/readers/diff-reader');
+const JSONWriter = require('../lib/writers/json');
+const Reconciler = require('../lib/index');
+const SQSOutput = require('../lib/output/sqs');
+const SQSWriter = require('../lib/writers/sqs');
+const StdoutOutput = require('../lib/output/stdout');
+
+program
+  .option('-d, --diff [filename]', 'Diff file')
+  .option('-f, --fxa [filename]', 'FxA CSV, format expected to be `uid,email,locale`')
+  .option('-s, --salesforce [filename]', 'Salesforce CSV, format expected to be `uid,email`')
+  .option('-j, --json', 'Write JSON output')
+  .option('-u, --url <SQS_url>', 'SQS URL')
+  .option('-r, --region <AWS_region>', 'AWS Region')
+  .option('--go', 'Send SQS messages, for real.');
+
+
+program.parse(process.argv);
+
+function usage () {
+  console.log(`Usage:
+  reconcile-csv -f <fxa_filename> -s <salesforce_filename> -u <SQS_url> -r <AWS_region>
+  reconcile-csv -d <diff_filename> -u <SQS_url> -r <AWS_region>
+`);
+}
+
+if (! ((program.diff || (program.fxa && program.salesforce)) && program.url && program.region)) {
+  usage();
+  process.exit(1);
+}
+
+
+let reader;
+if (program.fxa) {
+  const fxaInputPath = path.resolve(process.cwd(), program.fxa);
+  const salesforceInputPath = path.resolve(process.cwd(), program.salesforce);
+  reader = new CSVReader(fxaInputPath, salesforceInputPath, ',');
+} else {
+  const diffInputPath = path.resolve(process.cwd(), program.diff);
+  reader = new DiffReader(diffInputPath, ',');
+}
+
+let writer;
+if (program.json) {
+  writer = new JSONWriter(new StdoutOutput());
+} else {
+  let output;
+  if (!! program.go) {
+    output = new SQSOutput(program.region);
+  } else {
+    output = new StdoutOutput();
+  }
+  writer = new SQSWriter(program.url, output);
+}
+
+const reconciler = new Reconciler(program.url, program.region, reader, writer);
+reconciler.run();
