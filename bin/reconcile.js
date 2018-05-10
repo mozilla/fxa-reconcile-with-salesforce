8a7f86aecb3ad3bb9abd0d307218b6d2825a1e28
Shane Tomlinson <stomlinson@mozilla.com>
2018-05-10 14:01:22 +0100

    Kind of sort of have a thing that reads diffs and sends SQS messages.

--- bin/reconcile.js
+++ bin/reconcile.js
@@ -1,12 +1,31 @@
+const path = require('path');
 const program = require('commander');
+const Reconciler = require('../lib/index');
+const DiffReader = require('../lib/readers/diff-reader');
 
 program
-  .option('-f, --fxa', 'FxA user dB in CSV format')
-  .option('-s, --salesforce', 'Salesforce user dB in CSV format')
-  .option('-i, --keyid', 'Amazon access key id')
-  .option('-k, --key', 'Amazon secret access key')
+  .option('-i, --input <filename>', 'Differences file, expected to be output of diff')
+  .option('-u, --url <SQS_url>', 'SQS URL')
+  .option('-r, --region <AWS_region>', 'AWS Region')
   .option('--go', 'Do it, for real.');
 
 
 program.parse(process.argv);
 
+function usage () {
+  console.log(`Usage:
+  reconcile -i <filename> -u <SQS_url> -r <AWS_region>
+`);
+}
+
+if (! (program.input && program.url && program.region)) {
+  usage();
+  process.exit(1);
+}
+
+const inputPath = path.resolve(process.cwd(), program.input);
+
+const diffReader = new DiffReader(inputPath, ',');
+
+const reconciler = new Reconciler(program.url, program.region, diffReader, !! program.go);
+reconciler.run();
