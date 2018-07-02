c6631e7387c054973a8c2a4a6e93f61e58cf295c
Shane Tomlinson <stomlinson@mozilla.com>
2018-07-02 16:50:19 +0100

    feat(reconcile): Add scripts to delete/update FxA data from Salesforce data.

---
+++ bin/salesforce-update-emails.js
@@ -0,0 +1,59 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const path = require('path');
+const program = require('commander');
+
+const CSVReader = require('../lib/readers/csv');
+const SalesforceUpdateEmailsReconcilingStream = require('../lib/reconcilers/salesforce-update-emails');
+const StreamManager = require('../lib/reconcilers/stream-manager');
+const NewlineTransform = require('../lib/transforms/newline');
+const NullWriter = require('../lib/writers/null');
+
+program
+  .option('-e, --emails <filename>', 'Email updates CSV, format expected to be `uid,email`')
+  .option('-h, --highwater [N]', 'High water mark, defaults to 16384. See https://nodejs.org/api/stream.html#stream_constructor_new_stream_writable_options')
+  .option('-s, --salesforce <filename>', 'Salesforce CSV, format expected to be `uid,email`')
+  .option('--dryrun', 'Dry run, no output emitted');
+
+program.parse(process.argv);
+
+const programName = path.basename(process.argv[1]);
+
+function usage () {
+  console.log(`Usage:
+  node ${programName} -e <email_updates_csv_filename> -s <salesforce_csv_filename>
+`);
+}
+
+if (! program.emails || ! program.salesforce) {
+  usage();
+  process.exit(1);
+}
+
+const highWaterMark = parseInt(program.highwater || 16384, 10);
+
+const emailsInputPath = path.resolve(process.cwd(), program.emails);
+const salesforceInputPath = path.resolve(process.cwd(), program.salesforce);
+const readerStream = new CSVReader({
+  highWaterMark,
+  leftInputPath: emailsInputPath,
+  leftSource: 'update',
+  rightInputPath: salesforceInputPath,
+  rightSource: 'sf',
+  separator: ','
+});
+
+const reconcilerStream = new SalesforceUpdateEmailsReconcilingStream({
+  highWaterMark,
+});
+const newlineTransform = new NewlineTransform({
+  highWaterMark,
+});
+const outputStream = program.dryrun ? new NullWriter() : process.stdout;
+
+readerStream.pipe(reconcilerStream).pipe(newlineTransform).pipe(outputStream);
+
+const manager = new StreamManager(readerStream, outputStream, reconcilerStream, process.stderr); // eslint-disable-line no-unused-vars
+
